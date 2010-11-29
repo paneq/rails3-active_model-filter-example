ee8b263315acd140f907cca07c7b881a68fa042f
Robert Pankowecki (Gavdi) <rpa@gavdi.com>
2010-11-29 23:34:04 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+Car.create!(:producent => "Volkswagen", :brand => "Golf", :color => "ffffff", :max_speed => BigDecimal.new("170.10"), :valid_from => "2010-01-01", :valid_to => "2010-12-31")
+Car.create!(:producent => "Volkswagen", :brand => "Golf", :color => "000000", :max_speed => BigDecimal.new("189.10"), :valid_from => "2010-02-01", :valid_to => "2010-11-30")
+
+Car.create!(:producent => "Volkswagen", :brand => "Polo", :color => "ff0000", :max_speed => BigDecimal.new("140.60"), :valid_from => "2010-03-01", :valid_to => "2010-10-31")
+Car.create!(:producent => "Volkswagen", :brand => "Polo", :color => "00ff00", :max_speed => BigDecimal.new("101.10"), :valid_from => "2008-05-01", :valid_to => "2012-12-31")
+Car.create!(:producent => "Volkswagen", :brand => "Polo", :color => "0000ff", :max_speed => BigDecimal.new("111.30"), :valid_from => "2010-04-01", :valid_to => "2010-09-30")
+
+Car.create!(:producent => "Volkswagen", :brand => "Jetta", :color => "223344", :max_speed => BigDecimal.new("123.10"), :valid_from => "2010-07-01", :valid_to => "2011-03-31")
